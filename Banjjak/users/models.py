84966a85bebb44902731350b12e086c6378e7238
Yeinalee <yeinalee@gmail.com>
2023-08-11 03:56:01 +0900

    [Fix] 불필요한 user 필드 삭제

--- Banjjak/users/models.py
+++ Banjjak/users/models.py
@@ -8,13 +8,12 @@ from django.contrib.auth.models import AbstractUser, PermissionsMixin
 class User(AbstractUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)  # 반려인-닉네임, 관리자-보호소이름
     phone = models.CharField(verbose_name="전화번호", max_length=11)
     address = models.CharField(max_length=500)
     is_manager = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'  # 이메일을 메인키로 사용
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username']  # 반려인-닉네임, 관리자-보호소이름
 
     class Meta:
         verbose_name = 'user'
