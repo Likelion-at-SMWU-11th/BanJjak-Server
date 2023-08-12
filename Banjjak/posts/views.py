46ea6b65cd0fdafa1368d4eded5078b1312fa124
wonandonly <wanny416@sookmyung.ac.kr>
2023-08-12 21:14:37 +0900

    [feat] 관리자만 posts 쓸 수 있게 권한 수정

--- Banjjak/posts/views.py
+++ Banjjak/posts/views.py
@@ -1,12 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
 from .models import Post
 from .serializers import PostListSerializer
 
+class CanWritePostPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_write_permission()
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
-    permission_classes = [IsAuthenticated]  # 로그인 필수 설정
+    permission_classes = [IsAuthenticated, CanWritePostPermission]  # 로그인 필수 설정
 
     def perform_create(self, serializer):
-        serializer.save(writer=self.request.user)  # 작성자 자동 설정
+        serializer.save(writer=self.request.user)
