1a56ab99db3ad443b9e1aa92b832f0620946f0cb
Yeinalee <yeinalee@gmail.com>
2023-08-13 23:29:20 +0900

    [FEAT] 반려인 프로필 사진 등록, 업데이트 기능 구현 완료

--- Banjjak/users/models.py
+++ Banjjak/users/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser, PermissionsMixin):
     is_manager = models.BooleanField(default=False)  # 반려인-False, 관리자=True
     # 입양 절차 동의/동의-True, 비동의 - False
     is_agree = models.BooleanField(default=False)
+    profile = models.ImageField(verbose_name='프로필', blank=True)
 
     USERNAME_FIELD = 'email'  # 이메일을 메인키로 사용
     REQUIRED_FIELDS = ['username']  # 반려인-닉네임, 관리자-보호소이름
@@ -28,6 +29,6 @@ class User(AbstractUser, PermissionsMixin):
 
     def has_write_permission(self):
         return self.is_manager
-    
+
     def has_write_permission_user(self):
-         return self.is_manager==0 
+        return self.is_manager == 0
--- Banjjak/users/urls.py
+++ Banjjak/users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import userChange, ManagerChange, userChangePassword, managerChangePassword
+from .views import userChange, ManagerChange, userChangePassword, managerChangePassword, update_profile
 
 app_name = 'users'
 
 urlpatterns = [
     path("changeuserinfo/", userChange, name="change-user-info"),
-    path("changemanagerinfo/", ManagerChange, name="change-manager-info"),
     path("changeuserinfo/pw/", userChangePassword,
          name="change-user-password"),
+    path("changeuserinfo/profile/", update_profile,
+         name="change-user-profile"),
+    path("changemanagerinfo/", ManagerChange, name="change-manager-info"),
     path("changemanagerinfo/pw/", managerChangePassword,
          name="change-manager-password"),
 ]
