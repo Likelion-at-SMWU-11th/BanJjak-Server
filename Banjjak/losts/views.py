8e8b8fe7ac6c91d7c2ce1d2933e0635ad0dd0b6a
wonandonly <wanny416@sookmyung.ac.kr>
2023-08-14 03:28:00 +0900

    [feat] 동물 종류 분류 필터링

--- Banjjak/losts/views.py
+++ Banjjak/losts/views.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, status, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Lost
 from .serializers import LostListSerializer
+from rest_framework.decorators import action
 
 class CanWritePostPermission(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -22,14 +23,30 @@ class LostViewSet(viewsets.ModelViewSet):
     serializer_class = LostListSerializer
     permission_classes = [IsAuthenticated]  # 로그인 필수 설정
 
-    def get_permissions(self):
-        if self.action in ['create']:
-            self.permission_classes = [IsAuthenticated, CanWritePostPermission]
-        elif self.action in ['update', 'partial_update']:
-            self.permission_classes = [IsAuthenticated, CanEditPostPermission]
-        elif self.action == 'destroy':
-            self.permission_classes = [IsAuthenticated, CanDeletePostPermission]
-        return super().get_permissions()
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        animal_type = self.request.query_params.get('animal_type')  # URL 파라미터로 받은 카테고리
+        if animal_type:
+            queryset = queryset.filter(animal_type=animal_type)
+        return queryset
     
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update']:
+            return [IsAuthenticated(), CanEditPostPermission()]
+        elif self.action == 'destroy':
+            return [IsAuthenticated(), CanDeletePostPermission()]
+        elif self.action == 'create':
+            return [IsAuthenticated(), CanWritePostPermission()]
+        return []
+
+    @action(detail=True, methods=['GET'])
+    def detail(self, request, pk=None):
+        try:
+            lost = self.get_object()
+            serializer = self.get_serializer(lost)
+            return Response(serializer.data)
+        except Lost.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
     def perform_create(self, serializer):
         serializer.save(writer=self.request.user)  # 작성자 자동 설정
--- Banjjak/posts/views.py
+++ Banjjak/posts/views.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, status, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Post
 from .serializers import PostListSerializer
+from rest_framework.decorators import action
 
 class CanWritePostPermission(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -20,15 +21,33 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
     permission_classes = [IsAuthenticated]  # 로그인 필수 설정
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['animal_type']  # 검색 필드 지정
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        animal_type = self.request.query_params.get('animal_type')  # URL 파라미터로 받은 카테고리
+        if animal_type:
+            queryset = queryset.filter(animal_type=animal_type)
+        return queryset
 
     def get_permissions(self):
-        if self.action in ['create']:
-            self.permission_classes = [IsAuthenticated, CanWritePostPermission]
-        elif self.action in ['update', 'partial_update']:
-            self.permission_classes = [IsAuthenticated, CanEditPostPermission]
+        if self.action in ['update', 'partial_update']:
+            return [IsAuthenticated(), CanEditPostPermission()]
         elif self.action == 'destroy':
-            self.permission_classes = [IsAuthenticated, CanDeletePostPermission]
-        return super().get_permissions()
+            return [IsAuthenticated(), CanDeletePostPermission()]
+        elif self.action == 'create':
+            return [IsAuthenticated(), CanWritePostPermission()]
+        return []
+
+    @action(detail=True, methods=['GET'])
+    def detail(self, request, pk=None):
+        try:
+            post = self.get_object()
+            serializer = self.get_serializer(post)
+            return Response(serializer.data)
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
     def perform_create(self, serializer):
         serializer.save(writer=self.request.user)
