600f14f6b0639f8815a13fbc198f7eb3cb6bf07e
3eonah <3eonah.lee@gmail.com>
2023-08-17 23:08:23 +0900

    [FIX] writer_username추가

--- Banjjak/posts/serializers.py
+++ Banjjak/posts/serializers.py
@@ -5,7 +5,7 @@ from .models import Post
 
 class PostSerializer(ModelSerializer):
     hashtags = serializers.MultipleChoiceField(choices=Post.TAG_CHOICES)
-    # image1 = serializers.ImageField(use_url=True)
+    writer_username = serializers.ReadOnlyField(source='writer.username')  # 작성자의 username 필드
     class Meta:
         model = Post
         fields = '__all__'
